f1182db6c8d63368852af023123cc23bd2c92f93
William Schwindt <williammschwindt@gmail.com>
2020-01-30 17:51:26 -0800

    company onboarding from created

--- droom/src/components/CompanyOnboarding/CompanyOnboarding.js
+++ droom/src/components/CompanyOnboarding/CompanyOnboarding.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Form, Field, withFormik } from 'formik';
 
-const CompanyOnboarding = () => {
+const CompanyOnboarding = ({ errors, touched, values, status }) => {
     return (
-        <h1>CompanyOnboarding</h1>
+        <div>
+            <h1>CompanyOnboarding</h1>
+            <Form>
+                <Field name="username" type="text" value={values.username} placeholder="username" ></Field>
+                <Field name="password" type="text" value={values.password} placeholder="password" ></Field>
+                <Field name="area" type="text" value={values.area} placeholder="location" ></Field>
+                <Field name="bio" type="textarea" value={values.bio} placeholder="bio" ></Field>
+            </Form>
+        </div>
     )
 }
 
-export default CompanyOnboarding;
+const FormikCompanyOnboarding = withFormik({
+
+})(CompanyOnboarding);
+
+export default FormikCompanyOnboarding;
