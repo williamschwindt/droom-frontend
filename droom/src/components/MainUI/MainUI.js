79dac8541302e97e08b7eae74ef635940962bf87
William Schwindt <williammschwindt@gmail.com>
2020-02-03 18:02:51 -0800

    mainUI, matches, and profile components set up

---
+++ droom/src/components/MainUI/MainUI.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainUI = () => {
+    return (
+        <div>
+            <h1>MainUI</h1>
+        </div>
+    )
+}
+
+export default MainUI;
---
+++ droom/src/components/MatchesPages/MatchesPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MatchesPage = () => {
+    return (
+        <div>
+            <h1>Matches</h1>
+        </div>
+    )
+}
+
+export default MatchesPage;
