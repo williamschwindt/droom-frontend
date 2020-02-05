deda3728c3b153162c9ba874f0cfe9cd87aabd49
William Schwindt <williammschwindt@gmail.com>
2020-02-05 09:44:06 -0800

    updated nav bar

--- droom/src/components/MainUI/MainUI.js
+++ droom/src/components/MainUI/MainUI.js
@@ -3,15 +3,14 @@ import { Link } from 'react-router-dom';
 
 const MainUI = (props) => {
     return (
-        <div>
-            <h1>Droom</h1>
-            <h2>Find Your Droom Job!</h2>
-
+        <div className="main-ui-container">
             <nav>
                 <h3>Droom</h3>
-                <Link to="/seekerprofile">Profile</Link>
-                <Link to="/matches">Matches</Link>
-                <Link to="/home">Home</Link>
+                <div>
+                    <Link to="/seekerprofile">Profile</Link>
+                    <Link to="/matches">Matches</Link>
+                    <Link to="/home">Home</Link>
+                </div>
             </nav>
 
             
--- droom/src/components/MatchesPages/MatchesPage.js
+++ droom/src/components/MatchesPages/MatchesPage.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MatchesPage = () => {
     return (
-        <div>
-            <h1>Matches</h1>
+        <div className="matches-page-container">
+            <nav>
+                <h3>Droom</h3>
+                <div>
+                    <Link to="/seekerprofile">Profile</Link>
+                    <Link to="/matches">Matches</Link>
+                    <Link to="/home">Home</Link>
+                </div>
+            </nav>
+
+            
         </div>
     )
 }
