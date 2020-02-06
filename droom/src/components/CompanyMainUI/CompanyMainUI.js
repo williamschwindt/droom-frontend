04cb99e07e83617c567ac3a0cb44735cea0a46e7
Jensen Koch <jtkoch12@gmail.com>
2020-02-05 20:04:19 -0700

    Company Home page is displaying job seekers

--- droom/src/components/CompanyMainUI/CompanyMainUI.js
+++ droom/src/components/CompanyMainUI/CompanyMainUI.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const CompanyMainUI = (props) => {
+  const [seekers, setSeekers] = useState([]);
+
+  console.log(seekers);
+
+  useEffect(() => {
+    axios
+      .get("https://droom-node-server.herokuapp.com/api/seekers")
+
+      .then(res => {
+        console.log(res);
+        setSeekers(res.data);
+      })
+
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
   return (
     <div className="company-main-ui-container">
       <nav>
@@ -12,6 +31,26 @@ const CompanyMainUI = (props) => {
           <Link to="/CompanyMainUI">Home</Link>
         </div>
       </nav>
+
+      <div className="company-main-ui">
+        <h1>Find Employees</h1>
+        <div className="jobs">
+          {seekers.map(seekers => {
+            return (
+              <div key={seekers.id} className="job-card">
+                <h1>{seekers.name}</h1>
+                <h2>{seekers.location}</h2>
+                <div>
+                  <button>X</button>
+                  <button>Save</button>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+
+
     </div>
   )
 }
